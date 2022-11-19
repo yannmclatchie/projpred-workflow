0a16309e56d44639a3779816adab8acba51eb28d
fweber144 <fweber144@protonmail.com>
2022-11-19 20:55:40 +0200

    Cleanup (remove unused lines, store `brmsfit` in file, use multiple cores).

--- R/method-paths.R
+++ R/method-paths.R
@@ -1,5 +1,4 @@
 # load necessary libraries
-library(dplyr)
 library(brms)
 library(projpred)
 library(parallel)
@@ -7,51 +6,47 @@ library(parallel)
 # set seed
 SEED <- 1513306866
 
-# load data 
+# load data
 df <- read.table("./data/bodyfat/bodyfat.txt", header = T, sep = ";")
 df[,4:19] <- scale(df[,4:19])
 # no-one can have 0% body fat
 df <- df[df$siri>0,]
-y <- df[,"siri"]
 df <- as.data.frame(df)
 ( n <- nrow(df) )
 
 # define the covariates and variate
-pred <- c("age", "weight", "height", "neck", "chest", "abdomen", "hip", 
+pred <- c("age", "weight", "height", "neck", "chest", "abdomen", "hip",
           "thigh", "knee", "ankle", "biceps", "forearm", "wrist")
 target <- "siri"
 formula <- formula(paste("siri ~", paste(pred, collapse = " + ")))
-( p <- length(pred) )
 
 # fit the reference model
-p0 <- 5 # prior guess for the number of relevant variables
-tau0 <- p0/(p-p0) * 1/sqrt(n)
+options(mc.cores = detectCores(logical = FALSE))
 r2d2_prior <- set_prior(R2D2(mean_R2 = 0.3, prec_R2 = 3))
 # indep_gauss_prior <- set_prior("normal(0, 2.5)", class = "b")
 fit <- brm(
-  formula, 
-  data = df, 
+  formula,
+  data = df,
   prior = r2d2_prior, # prior = indep_gauss_prior,
-  seed = SEED, 
-  refresh = 0
+  seed = SEED,
+  refresh = 0,
+  file = "bodyfat_fit",
+  file_refit = "on_change"
 )
 
 # perform projpred with forward and L1 search
 vs_forward <- cv_varsel(
-  fit, 
-  method = 'forward',
-  seed = SEED, 
-  verbose = FALSE
+  fit,
+  method = "forward",
+  seed = SEED
 )
 vs_l1 <- cv_varsel(
-  fit, 
-  method = 'l1',
-  seed = SEED, 
-  verbose = FALSE
+  fit,
+  method = "l1",
+  seed = SEED
 )
 
 # plot the stability of selection process
 ## ----
 ## TODO
 ## ----
-
