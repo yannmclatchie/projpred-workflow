799cb780e42aeabd55e58359b19948f0c3910071
fweber144 <fweber144@protonmail.com>
2022-11-19 20:55:48 +0200

    Add `R/aux/gg_pct_solution_terms_cv.R`.

---
+++ R/aux/gg_pct_solution_terms_cv.R
@@ -0,0 +1,34 @@
+# Source:
+# <https://github.com/fweber144/modelselection/blob/9e65616c6bf9e234747b2e97a3f602cb5c264c92/bodyfat.Rmd>,
+# which in turn is based on
+# <https://github.com/avehtari/modelselection/blob/ad0e10ce7d3a47de560609db7abe83777eed28bc/bodyfat.Rmd>.
+
+gg_pct_solution_terms_cv <- function(cvvs) {
+  pct_cum <- apply(cvvs[["pct_solution_terms_cv"]][, -1], 2, cumsum)
+  pct_cum <- cbind(cvvs[["pct_solution_terms_cv"]][, 1, drop = FALSE], pct_cum)
+  rows <- nrow(pct_cum)
+  col <- nrow(pct_cum)
+  pctch <- round(pct_cum, 2)
+  colnames(pctch)[1] <- ".size"
+  pct <- get_pct_arr(pctch, 13)
+  col_brks <- get_col_brks()
+  pct$val_grp <- as.character(sapply(pct$val, function(x) sum(x >= col_brks$breaks)))
+  if (identical(rows, 0)) rows <- pct$var[1]
+  pct$sel <- (pct$.size == col) & (pct$var %in% rows)
+  brks <- sort(unique(as.numeric(pct$val_grp)) + 1)
+  ggobj <- ggplot(pct, aes_(x = ~.size, y = ~var)) +
+    geom_tile(aes_(fill = ~val_grp, color = ~sel),
+              width = 1, height = 0.9, size = 1) +
+    geom_text(aes_(label = ~val, fontface = ~sel+1)) +
+    coord_cartesian(expand = FALSE) +
+    scale_y_discrete(limits = rev(levels(pct$var))) +
+    scale_x_discrete(limits = factor(seq(1,col))) +
+    scale_color_manual(values = c("white", "black")) +
+    labs(x = "Model size", y = "",
+         title = "Fraction of cv-folds that select the given variable") +
+    scale_fill_manual(breaks = brks, values = col_brks$pal[brks]) +
+    theme_proj() +
+    theme(legend.position = "none",
+          axis.text.y = element_text(angle = 45))
+  return(ggobj)
+}
