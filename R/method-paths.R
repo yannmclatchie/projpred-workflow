4d1ded20c3438f480ab8a737042b2a71d5d190c6
fweber144 <fweber144@protonmail.com>
2022-11-21 17:03:12 +0100

    `R/method-paths.R`: Add side-by-side figure for comparing forward search and L1 search, now
    with fixed "limits" (here: annotation) on the y-axis.

--- R/method-paths.R
+++ R/method-paths.R
@@ -59,6 +59,8 @@ source("./R/aux/gg_pct_solution_terms_cv.R")
 ( gg_l1 <- gg_pct_solution_terms_cv(vs_l1) )
 library(patchwork)
 ( gg_fw_l1 <- gg_fw | gg_l1 )
+y_fw_order <- gg_fw_build$layout$panel_scales_y[[1]]$range$range
+( gg_fw_l1_fixedY <- (gg_fw | gg_l1) & scale_y_discrete(limits = y_fw_order) )
 source("./R/aux/aux_plotting.R")
 save_tikz_plot(plot = gg_fw,
                filename = "./tex/pct_solution_terms_cv_forward.tex",
@@ -68,4 +70,7 @@ save_tikz_plot(plot = gg_l1,
                width = 6)
 save_tikz_plot(plot = gg_fw_l1,
                filename = "./tex/pct_solution_terms_cv_forward_l1.tex",
+               width = 6)
+save_tikz_plot(plot = gg_fw_l1_fixedY,
+               filename = "./tex/pct_solution_terms_cv_forward_l1_fixedY.tex",
                width = 6)
