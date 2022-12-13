0c0cdbe4c62dda90c5a0969b005ec90d4f644be6
Sölvi Rögnvaldsson <sor16@hi.is>
2022-12-13 10:26:19 +0200

    changed default parameter number to be 50 and fixed some bugs and added more run time info

--- R/case_study_weakly_relevant.R
+++ R/case_study_weakly_relevant.R
@@ -5,35 +5,37 @@ library(argparser)
 source('R/generate.R')
 #source('R/aux/aux_plotting.R')
 ## ----- Define argument parser -------
-p <- arg_parser("weakly relevant predictors case study from the paper 'Robust and efficient projection predictive inference'")
-p <- add_argument(p, "--R2", help="R2 in the simulated data",type="numeric")
-p <- add_argument(p, "--fix_beta", help="Chould betas all be equal?",type="logical")
-p <- add_argument(p, "--rho", help="correlation coefficients between the predictors",type="numeric")
+pars <- arg_parser("weakly relevant predictors case study from the paper 'Robust and efficient projection predictive inference'")
+pars <- add_argument(pars, "--R2", help="R2 in the simulated data",type="numeric")
+pars <- add_argument(pars, "--fix_beta", help="Chould betas all be equal?",type="logical")
+pars <- add_argument(pars, "--rho", help="correlation coefficients between the predictors",type="numeric")
 
-args <- parse_args(p)
+args <- parse_args(pars)
 set.seed(4321)
+t <- Sys.time()
 rho <- args$rho
 n <- 500
-p <- 100
+p <- 50
 R2 <- args$R2
 sigma <- 1
 sim <- generate_fixed_R2_data(n=n,p=p,sigma=sigma,R2=args$R2,rho=args$rho,fixed_beta = args$fix_beta)
 dat <- sim$df
 beta <- sim$beta
 prior_ref <- prior(R2D2(mean_R2=0.3,prec_R2=5,cons_D2=10))
-mod_ref <- brm(as.formula(paste0('y~',paste(paste0('X',1:100),collapse='+'))),data=dat,prior=prior_ref,family=gaussian(),refresh=0,iter=2000,cores=4)
+mod_ref <- brm(as.formula(paste0('y~',paste(paste0('X',1:p),collapse='+'))),data=dat,prior=prior_ref,family=gaussian(),refresh=0,iter=2000,cores=4)
 
 # rhats <- rhat(mod_ref)
 # loo_ref <- loo(mod_ref)
 # neff_ratio(mod_ref)
 # elpd_ref <- loo_ref$estimates["elpd_loo", "Estimate"]
 
-vs_forward_loo <- cv_varsel(mod_ref,cv_method='LOO',method = "forward",validate_search=F,nterms_max = 100)
+vs_forward_loo <- cv_varsel(mod_ref,cv_method='LOO',method = "forward",validate_search=F,nterms_max = p)
 size_maximum_elpd <- vs_forward_loo$summary$size[which.max(vs_forward_loo$summary$elpd.loo)]
+print(paste('Running cross validated search with nterms_max = ',size_maximum_elpd))
 vs_forward_loo_validated <- cv_varsel(mod_ref,cv_method='LOO',method = "forward",validate_search=T,nterms_max = size_maximum_elpd)
 save(vs_forward_loo,vs_forward_loo_validated,
      file=paste0('results/varsel_objects_R2_',args$R2,'_rho_',args$rho,'_fix_beta',args$fix_beta,'.RData'))
-
+print(Sys.time()-t)
 # mod_size_candidates <- c('confidence_heuristic'=suggest_size(vs_forward_loo),
 #                         'elpd_difference_heuristic'=suggest_size(vs_forward_loo,thres_elpd = -4))
 #
