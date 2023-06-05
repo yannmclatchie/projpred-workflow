922c67fa6174278967883e9a3f693de3a6991a67
Sölvi Rögnvaldsson <solviro@gmail.com>
2023-06-05 22:59:51 +0300

    adjusted loo experiment function so it compares normal, r2d2 and r2d2normal reference models

--- R/SBC_experiment/loo_experiment_functions.R
+++ R/SBC_experiment/loo_experiment_functions.R
@@ -1,12 +1,14 @@
-run_loo_experiment <- function(N_sim,N,n_rel,n_irrel,prior_ref){
+library(brms)
+source('R/SBC_experiment/SBC_experiment_functions.R')
+run_loo_experiment <- function(N_sim,N,n_rel,n_irrel){
   covars <- paste0('x',seq_len(n_rel+n_irrel))
   rel_covars <- paste0('x',seq_len(n_rel))
   rel_covars_str <- paste(rel_covars,collapse='+')
   ref_mod_vars_str <- paste(c('t',covars),collapse='+')
   ref_mod_formula <- paste0('y~',ref_mod_vars_str)
-  treatment_post_list <- list()
-  sigma_post_list <- list()
-  run_info_list <- list()
+  loo_normal <- vector('numeric',N_sim)
+  loo_r2d2 <- vector('numeric',N_sim)
+  loo_r2d2normal <- vector('numeric',N_sim)
   for(i in 1:N_sim){
     print(paste('Iteration:',i))
     seed_iter <- sample(1:1e8,size = 1)
@@ -17,57 +19,37 @@ run_loo_experiment <- function(N_sim,N,n_rel,n_irrel,prior_ref){
     sigma <- sim$sigma
     #compile reference model in first iteration
     if(i==1){
-      if(prior_ref=='normal'){
-        prior_mod <- prior(normal(0,1),class="b") + prior(exponential(1),class='sigma')
-        mod_ref <- brm(ref_mod_formula,data=dat,prior=prior_mod,family=gaussian(),refresh=0,iter=2000,cores=4, save_pars = save_pars(all = TRUE))
-      }else if(prior_ref=='normalr2d2'){
-        prior_mod <- prior(R2D2(mean_R2=0.3,prec_R2=20,cons_D2 = 0.2)) + prior(exponential(1),class='sigma')
-        mod_ref <- brm(ref_mod_formula,data=dat,prior=prior_mod,family=gaussian(),refresh=0,chains=0,iter=16000,thin=8,cores=4,control=list(adapt_delta=0.999), save_pars = save_pars(all = TRUE))
-        mod_ref_standata <- make_standata(ref_mod_formula,data=dat,prior=prior_mod)
+        #reference model with independent normal prior
+        prior_mod_normal <- prior(normal(0,1),class="b") + prior(exponential(1),class='sigma')
+        mod_ref_normal <- brm(ref_mod_formula,data=dat,prior=prior_mod_normal,family=gaussian(),refresh=0,iter=2000,cores=4, save_pars = save_pars(all = TRUE))
+        #reference model with r2d2 prior on all coefficients
+        prior_mod_r2d2 <- prior(R2D2(mean_R2=0.3,prec_R2=20,cons_D2 = 0.2)) + prior(exponential(1),class='sigma')
+        mod_ref_r2d2 <- brm(ref_mod_formula,data=dat,prior=prior_mod_r2d2,family=gaussian(),refresh=0,chains=4,iter=2000,thin=2,cores=4,control=list(adapt_delta=0.999), save_pars = save_pars(all = TRUE))
+
+        #reference model with normal prior on treatment and r2d2 prior on the rest
+        mod_ref_r2d2normal<- brm(ref_mod_formula,data=dat,prior=prior_mod_r2d2,family=gaussian(),refresh=0,chains=0,iter=16000,thin=8,cores=4,control=list(adapt_delta=0.999), save_pars = save_pars(all = TRUE))
+        mod_ref_standata <- make_standata(ref_mod_formula,data=dat,prior=prior_mod_r2d2)
         mod_ref_modified <- rstan::stan(file = 'stan/r2d2_normal.stan', data = mod_ref_standata,refresh=0)
-        mod_ref$fit <- mod_ref_modified
-        mod_ref <- rename_pars(mod_ref)
-      }else{
-        stop('Prior not supported')
-      }
-      mod_ref_base <- mod_ref
+        mod_ref_r2d2normal$fit <- mod_ref_modified
+        mod_ref_r2d2normal <- rename_pars(mod_ref_r2d2normal) # note: warning message is because one concentration parameter was removed. Model object stays intact
+        #store the compiled models to be used later
+        mod_ref_normal_base <- mod_ref_normal
+        mod_ref_r2d2_base <- mod_ref_r2d2
+        mod_ref_r2d2normal_base <- mod_ref_r2d2normal
     }else{
-      mod_ref <- update(mod_ref_base,newdata=dat,recompile=F,refresh=0)
+      mod_ref_normal <- update(mod_ref_normal_base,newdata=dat,recompile=F,refresh=0)
+      mod_ref_r2d2 <- update(mod_ref_r2d2_base,newdata=dat,recompile=F,refresh=0)
+      mod_ref_r2d2normal <- update(mod_ref_r2d2normal_base,newdata=dat,recompile=F,refresh=0)
     }
-    loo_mod_ref <- loo(mod_ref, moment_match = TRUE, reloo = TRUE)
-    # TODO: Does the current code allow to compute elpd difference between different `prior_ref`s but for the same dataset, i.e., the same SBC iteration (after storing `loo_mod_ref_clean` in the results, I guess)?
-    mod_names <- c('mod_ref','prj_sel','mod_refit_prj')
-    mods <- list(mod_ref)
-
-    treatment_post_list[[i]] <- lapply(mods,function(m) tibble(value=as.matrix(m)[,'b_t'])) %>%
-                         bind_rows(.id='model_nr') %>%
-                         mutate(model=mod_names[as.integer(model_nr)],
-                                sim=i,
-                                prior_draw=beta['t']) %>%
-                         dplyr::select(sim,model,prior_draw,value)
-    sigma_post_list[[i]] <- lapply(mods,function(m) tibble(value=as.matrix(m)[,'sigma'])) %>%
-                            bind_rows(.id='model_nr') %>%
-                            mutate(model=mod_names[as.integer(model_nr)],
-                                   sim=i,
-                                   prior_draw=sigma) %>%
-                            dplyr::select(sim,model,prior_draw,value)
-
-    #Store model diagnostics of the reference model as well as other run info
-    mod_ref_summary <- summary(mod_ref)
-    run_info_list[[i]] <- bind_cols(tibble(sim=i,seed=seed_iter),
-                                    tibble(snr=sigma^2/var(dat$y),
-                                           max_r_hat = min(mod_ref_summary$fixed$Rhat),
-                                           min_bulk_ess_ref = min(mod_ref_summary$fixed$Bulk_ESS),
-                                           min_tail_ess_ref = min(mod_ref_summary$fixed$Tail_ESS),
-                                           num_divergent_ref = rstan::get_num_divergent(mod_ref$fit),
-                                           num_high_k_value = sum(loo_mod_ref$diagnostics$pareto_k>0.7)),
-                                    as_tibble(t(c(beta,sigma))),
-                                    tibble(full_mod=ref_mod_vars_str))
+    loo_mod_ref_normal <- loo(mod_ref_normal, moment_match = TRUE, reloo = TRUE)
+    loo_mod_ref_r2d2 <- loo(mod_ref_r2d2) #note: moment_match gives error, indicating not all parameters were stored (which they in fact were!)
+    loo_mod_ref_r2d2normal <- loo(mod_ref_r2d2normal) #note: moment_match gives error, indicating not all parameters were stored (which they in fact were!)
+    loo_normal[i] <- loo_mod_ref_normal$estimates['elpd_loo','Estimate']
+    loo_r2d2[i] <- loo_mod_ref_r2d2$estimates['elpd_loo','Estimate']
+    loo_r2d2normal[i] <- loo_mod_ref_r2d2normal$estimates['elpd_loo','Estimate']
   }
-  treatment_post_dat <- bind_rows(treatment_post_list)
-  sigma_post_dat <- bind_rows(sigma_post_list)
-  treatment_summary <- get_posterior_summary(treatment_post_dat)
-  sigma_summary <- get_posterior_summary(sigma_post_dat)
-  run_info <- bind_rows(run_info_list)
-  return(list(treatment_summary=treatment_summary,sigma_summary=sigma_summary,run_info=run_info, loo_mod_ref = loo_mod_ref))
+  return(data.frame(iter=seq_len(N_sim),loo_normal=loo_normal,loo_r2d2=loo_r2d2,loo_r2d2normal=loo_r2d2normal))
 }
+
+#Try running one iteration of loo experiment
+run_loo_experiment(N_sim=1,N=100,n_rel=60,n_irrel=10)
