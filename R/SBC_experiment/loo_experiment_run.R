bc2033994277b797ddb1da5295256ac7ffaacbfc
Sölvi Rögnvaldsson <solviro@gmail.com>
2023-06-28 12:37:16 +0300

    Some file renaming and organization of loo experiment code

--- R/SBC_experiment/loo_experiment_run.R
+++ R/SBC_experiment/loo_experiment_run.R
@@ -1,46 +1,71 @@
-## ----- Setup -------
-#move from sbatch directory to project directory
-setwd('../../')
-#Tell cluster where R is located
-.libPaths('../R/x86_64-pc-linux-gnu-library/4.1/')
 library(brms)
-library(projpred)
 library(dplyr)
-library(argparser)
+options(brms.backend = "rstan")
 source('R/SBC_experiment/SBC_experiment_functions.R')
-source('R/SBC_experiment/loo_experiment_functions.R')
-## ----- Define argument parser -------
-p <- arg_parser("Run loo for the SBC experiment from the paper 'Robust and efficient projection predictive inference'")
-p <- add_argument(p, "--N_sim", help="Number of SBC simulations to perform", default=10,type="numeric")
-p <- add_argument(p, "--N", help="Integer determining the size of the simulated data sets",type="numeric")
-p <- add_argument(p, "--n_rel", help="Integer determining the number of relevant predictors",type="numeric")
-p <- add_argument(p, "--n_irrel", help="Integer determining the number of irrelevant predictors",type="numeric")
-p <- add_argument(p, "--prior_ref", help="Character denoting prior of the beta coefficients in the reference model.
-                                          Supports values 'normal' and 'normalr2d2', where normal r2d2 refers to a
-                                          normal prior on treatment and r2d2 on remainin covariates", default='normal',type="character")
-p <- add_argument(p, "--experiment_suffix", help="Suffix to add to file path to label experiment.", default="",type="character")
-p <- add_argument(p, "--path", help="Path were results should be saved", default="",type="character")
+run_loo_experiment <- function(N_sim,N,n_rel,n_irrel){
+  covars <- paste0('x',seq_len(n_rel+n_irrel))
+  rel_covars <- paste0('x',seq_len(n_rel))
+  rel_covars_str <- paste(rel_covars,collapse='+')
+  ref_mod_vars_str <- paste(c('t',covars),collapse='+')
+  ref_mod_formula <- paste0('y~',ref_mod_vars_str)
+  loo_results <- tibble()
+  for(i in 1:N_sim){
+    print(paste('Iteration:',i))
+    seed_iter <- sample(1:1e8,size = 1)
+    print(paste0('seed: ',seed_iter))
+    sim <- def_indep_treatment_experiment(N = N,n_rel = n_rel, n_irrel = n_irrel, seed = seed_iter)
+    dat <- sim$dat
+    beta <- sim$beta
+    sigma <- sim$sigma
+    #compile reference model in first iteration
+    if(i==1){
+        #reference model with independent normal prior
+        prior_mod_normal <- prior(normal(0,1),class="b") + prior(exponential(1),class='sigma')
+        mod_ref_normal <- brm(ref_mod_formula,data=dat,prior=prior_mod_normal,family=gaussian(),refresh=0,iter=2000,cores=4, save_pars = save_pars(all = TRUE))
+        # rstan::check_hmc_diagnostics(mod_ref_normal$fit)
+        #reference model with r2d2 prior on all coefficients
+        #prior_mod_r2d2 <- prior(R2D2(mean_R2=0.3,prec_R2=20,cons_D2 = 0.2)) + prior(exponential(1),class='sigma')
+        prior_mod_r2d2 <- prior(R2D2(mean_R2=0.3,prec_R2=20,cons_D2 = 0.2)) + prior(exponential(1),class='sigma')
+        mod_ref_r2d2 <- brm(ref_mod_formula,data=dat,prior=prior_mod_r2d2,family=gaussian(),refresh=0,chains=4,iter=4000,thin=4,cores=4,control=list(adapt_delta=0.999), save_pars = save_pars(all = TRUE))
+        # rstan::check_hmc_diagnostics(mod_ref_r2d2$fit)
 
-args <- parse_args(p)
+        #reference model with normal prior on treatment and r2d2 prior on the rest
+        mod_ref_r2d2normal<- brm(ref_mod_formula,data=dat,prior=prior_mod_r2d2,family=gaussian(),refresh=0,chains=0,iter=4000,thin=4,cores=4,control=list(adapt_delta=0.999), save_pars = save_pars(all = TRUE))
+        mod_ref_standata <- make_standata(ref_mod_formula,data=dat,prior=prior_mod_r2d2)
+        mod_ref_modified <- rstan::stan(file = 'stan/r2d2_normal.stan', data = mod_ref_standata,refresh=0)
+        # rstan::check_hmc_diagnostics(mod_ref_modified)
+        mod_ref_r2d2normal$fit <- mod_ref_modified
+        mod_ref_r2d2normal <- rename_pars(mod_ref_r2d2normal) # note: warning message is because one concentration parameter was removed. Model object stays intact
+        #store the compiled models to be used later
+        mod_ref_normal_base <- mod_ref_normal
+        mod_ref_r2d2_base <- mod_ref_r2d2
+        mod_ref_r2d2normal_base <- mod_ref_r2d2normal
+    }else{
+      mod_ref_normal <- update(mod_ref_normal_base,newdata=dat,recompile=F,refresh=0)
+      mod_ref_r2d2 <- update(mod_ref_r2d2_base,newdata=dat,recompile=F,refresh=0)
+      mod_ref_r2d2normal <- update(mod_ref_r2d2normal_base,newdata=dat,recompile=F,refresh=0)
+    }
+    loo_mod_ref_normal <- loo(mod_ref_normal, moment_match = TRUE, reloo = TRUE)
+    loo_mod_ref_r2d2 <- loo(mod_ref_r2d2, reloo = TRUE)
+    loo_mod_ref_r2d2normal <- loo(mod_ref_r2d2normal, reloo = TRUE)
+    loo_normal_vs_r2d2 <- loo_compare(loo_mod_ref_normal,loo_mod_ref_r2d2)
+    loo_normal_vs_r2d2normal <- loo_compare(loo_mod_ref_normal,loo_mod_ref_r2d2normal)
+    r2=summary(lm(as.formula(paste0('y~t+',paste(paste0('x',1:n_rel),collapse='+'))),data=dat))$r.squared
+    loo_results <- bind_rows(loo_results,tibble(iter=i,
+                                         r2=r2,
+                                         elpd_ref_normal=loo_mod_ref_normal$estimates['elpd_loo','Estimate'],
+                                         normal_r2d2_loser=rownames(loo_normal_vs_r2d2)[2],
+                                         elpd_diff_normal_r2d2=loo_normal_vs_r2d2[2,'elpd_diff'],
+                                         se_diff_normal_r2d2=loo_normal_vs_r2d2[2,'se_diff'],
+                                         normal_r2d2normal_loser=rownames(loo_normal_vs_r2d2normal)[2],
+                                         elpd_diff_normal_r2d2normal=loo_normal_vs_r2d2normal[2,'elpd_diff'],
+                                         se_diff_normal_r2d2normal=loo_normal_vs_r2d2normal[2,'se_diff']))
+  }
+  return(loo_results)
+}
 
-## ----- Run experiment -------
-# args <- list()
-# args$N_sim <- 1
-# args$N <- 100
-# args$n_rel <- 10
-# args$n_irrel <- 10
-# args$prior_ref <- 'normal'
-# args$experiment_suffix <- ''
-# args$path <- 'results/'
 
-experiment <- run_loo_experiment(N_sim=args$N_sim,N=args$N,n_rel=args$n_rel,n_irrel=args$n_irrel,prior_ref=args$prior_ref)
-
-experiment_name <- paste0(args$N_sim,'sim_',
-                          args$N,'N_',
-                          args$n_rel,'rel_',
-                          args$n_irrel,'irrel_',
-                          args$prior_ref,'prior',
-                          ifelse(args$experiment_suffix!='',paste0('_',args$experiment_suffix),''))
-write.table(experiment$treatment_summary,file=paste0(args$path,'treatment_summary_',experiment_name,'.tsv'),quote=F,row.names=F,sep='\t')
-write.table(experiment$sigma_summary,file=paste0(args$path,'sigma_summary_',experiment_name,'.tsv'),quote=F,row.names=F,sep='\t')
-write.table(experiment$run_info,file=paste0(args$path,'run_info_',experiment_name,'.tsv'),quote=F,row.names=F,sep='\t')
+#Try running one iteration of loo experiment
+set.seed(1234)
+loo_results <- run_loo_experiment(N_sim=10,N=100,n_rel=10,n_irrel=60)
+write.table(loo_results,'loo_experiment_r2d2_0.3_20_0.2.tsv',quote = F,row.names=F,sep = '\t')
